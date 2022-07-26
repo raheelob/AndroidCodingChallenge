cf9850003ea6e554cf4b71dcec34d22913f3641c
raheelob <raheel.ob@gmail.com>
2022-07-27 01:07:51 +0400

    Unused Resources removed
    Unused Resources removed

--- app/src/main/java/com/example/androidcodingchallenge/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.androidcodingchallenge.utils
-
-object Constants {
-
-}
--- app/src/main/java/com/example/androidcodingchallenge/utils/EnumsCollection.kt
+++ app/src/main/java/com/example/androidcodingchallenge/utils/EnumsCollection.kt
@@ -1,6 +1,6 @@
 package com.example.androidcodingchallenge.utils
 
 enum class ProductItemType(val status: Int) {
-    VIEW_TYPE_AVAILABLE(3),
-    VIEW_TYPE_UNAVAILABLE(4),
+    VIEW_TYPE_AVAILABLE(0),
+    VIEW_TYPE_UNAVAILABLE(1),
 }
